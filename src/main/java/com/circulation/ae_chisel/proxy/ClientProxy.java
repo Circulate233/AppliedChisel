953df03a173c455e361d55cd57870cc2bf6cb74a
Circulate233 <a18966009058@163.com>
2025-10-13 22:39:34 +0800

    init

---
+++ src/main/java/com/circulation/ae_chisel/proxy/ClientProxy.java
@@ -0,0 +1,20 @@
+package com.circulation.ae_chisel.proxy;
+
+public class ClientProxy extends CommonProxy {
+
+    @Override
+    public void preInit() {
+        super.preInit();
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void postInit() {
+        super.postInit();
+    }
+
+}
---
+++ src/main/java/com/circulation/ae_chisel/proxy/CommonProxy.java
@@ -0,0 +1,14 @@
+package com.circulation.ae_chisel.proxy;
+
+public class CommonProxy {
+
+    public void preInit() {
+    }
+
+    public void init() {
+    }
+
+    public void postInit() {
+
+    }
+}
